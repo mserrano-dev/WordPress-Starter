55551862a90b4da1ccab4cd4f1ff75fcc340b614
Mark Anthony Serrano <markanthony.serrano2@gmail.com>
2020-09-30 11:08:42 -0700

    Add wordpress updater tool. Finish re-base of build tools to root dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
-require('dotenv').config() // load .env file into process.env
+require('dotenv').config(); // load .env file into process.env
 const { src, dest, series, parallel } = require('gulp');
 const webpack = require('webpack-stream'); // gulp task runner with webpack bundler
+const named = require('vinyl-named'); // multiple named webpack entry files
 const replace = require('gulp-replace'); // dynamically create the style.css file
+const del = require('del'); // delete files/folders
+const ncp = require('ncp').ncp; // copy directories recursively (move wp-admin/wp-include to our code base)
+const fs = require('fs'); // write to filesystem
+const AdmZip = require('adm-zip'); // unzip
+const axios = require('axios'); // make ajax call to download latest wordpress install
+const ProgressBar = require('progress'); // ascii progress indicator
 const browserSync = require('browser-sync').create(); // inject any asset changes
 const argv = require('yargs').argv; // used by devops.js: (--mode=git|sync|dev|build)
 const {
@@ -12,10 +19,71 @@ const {
 } = require('./project-settings');
 
 // --- Gulp4 Tasks --- //
+function wordpress_installSetUp() {
+  // clean up current installation
+  return del([
+    'wp-admin/',
+    'wp-includes/',
+  ]);
+}
+
+function wordpress_downloadLatest(done) {
+  // clean up current installation
+  del([
+    'wp-admin/',
+    'wp-includes/',
+  ]);
+
+  // download the latest
+  console.log('Connecting …');
+  axios({
+    url: 'https://wordpress.org/latest.zip',
+    method: 'GET',
+    responseType: 'stream',
+    headers: {
+      'Content-Type': 'application/zip'
+    }
+  }).then((response) => {
+    const totalLength = response.headers['content-length'];
+    const progressBar = new ProgressBar('-> downloading [:bar] :percent :etas', {
+      width: 40,
+      complete: '=',
+      incomplete: ' ',
+      renderThrottle: 1,
+      total: parseInt(totalLength)
+    });
+    response.data.on('data', (chunk) => progressBar.tick(chunk.length));
+    response.data.pipe(fs.createWriteStream('wordpress.zip'));
+    response.data.on('end', done);
+  });
+}
+
+function wordpress_unzipAndInstall(done) {
+  var zip = new AdmZip("wordpress.zip");
+  zip.extractAllTo('.');
+  del([
+    'wordpress/wp-content'
+  ]);
+
+  ncp.limit = 16;
+
+  ncp('wordpress', '.', function (err) {
+    done();
+  });
+}
+
+function wordpress_installTearDown() {
+  return del([
+    'wordpress.zip',
+    'wordpress'
+  ]);
+}
+
 function do_webpack() {
-  return src([project.scripts.entry, project.styles.entry])
-    .pipe(webpack( require('./webpack.config.js') ))
-    .pipe(dest(`assets`));
+  return src([`wp-content/themes/src/_dev/${project.entry.main}.ts`, `wp-content/themes/src/_dev/${project.entry.admin}.ts`])
+    .pipe(named())
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(dest(`wp-content/themes/src/assets`));
 }
 
 function init_browserSync(done) {
@@ -23,9 +91,10 @@ function init_browserSync(done) {
     proxy: project.dev_vhost,
     open: 'external',
     files: [
-      './assets/' + project.scripts.filename, 
-      './assets/' + project.styles.filename,
-      '**/*.php'
+      `assets/${project.entry.admin}.min.js`,
+      `assets/${project.entry.main}.min.css`,
+      `assets/${project.entry.admin}.min.css`,
+      '**/*.*',
     ],
     snippetOptions: {
       rule: {
@@ -48,15 +117,15 @@ function prepare_deployment() {
     '!node_modules',
     '!node_modules/**/*',
     '!.env',
-    '!style.css',
+    '!wp-content/themes/src/style.css',
   ];
-  return src(['./**/*'].concat(list_ignore))
+  return src(['wp-content/themes/src/**/*'].concat(list_ignore))
     .pipe(dest(versioned_deployment_pkg));
 }
 
 function generate_wordpress_meta() {
   // dynamically generate the style.css file
-  return src('./style.css')
+  return src('wp-content/themes/src/style.css')
     .pipe(replace(/\%_VERSION_\%/g, package_info.version))
     .pipe(replace(/\%_NAME_\%/g, package_info.name))
     .pipe(replace(/\%_AUTHOR_\%/g, package_info.author))
@@ -68,6 +137,14 @@ switch (argv.mode) {
   case 'git':
     exports.default = series(
       update_git_config,
+    );
+    break;
+  case 'updateWP':
+    exports.default = series(
+      wordpress_installSetUp,
+      wordpress_downloadLatest,
+      wordpress_unzipAndInstall,
+      wordpress_installTearDown
     );
     break;
   case 'dev':
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin'); // typescr
 const globImporter = require('node-sass-glob-importer'); // sass glob imports
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // minify
 const autoprefixer = require('autoprefixer'); // config at package.json Browserslist
-const { 
+const {
   env, // boolean flags for production, development
   project // variable filenames, paths, etc
 } = require('./project-settings');
@@ -11,13 +11,19 @@ module.exports = {
   mode: (env.development ? 'development' : 'production'),
   watch: env.development,
   devtool: (env.development ? 'inline-source-map' : ''),
-  output: {filename: project.scripts.filename},
+  output: {
+    filename: '[name].min.js'
+  },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader'
       },
       {
         test: /\.s[ac]ss$/i,
@@ -47,20 +53,22 @@ module.exports = {
                 importer: globImporter(),
               }
             }
-          }
+          },
         ]
       }
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: project.styles.filename }),
+    new MiniCssExtractPlugin({
+      filename: '[name].min.css'
+    }),
   ],
   optimization: {
     usedExports: false,
     minimize: env.production
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: ['.tsx', '.ts', '.js'],
     alias: {
       vue: (env.production ? 'vue/dist/vue.min.js' : 'vue/dist/vue.js'),
     },
